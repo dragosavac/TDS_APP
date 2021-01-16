270a539e65f48c2721b35aaf3fac16376a669abc
Rade <rade.dragosavac@etondigital.com>
2021-01-16 13:42:08 +0100

    small fix

--- tds_project/settings.py
+++ tds_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "qzru#t9g1t=4a#ae-k!l2(gfg(i1r@(_i0^vyxx&75)_r0yg+h"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'tds_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "new_tds",
-        'HOST': "localhost",
-        'USER': "new_tds",
-        'PASSWORD': "new_tds",
-        'PORT': "5432",
+        'NAME': os.environ.get("DB_NAME"),
+        'HOST': os.environ.get("DB_HOST"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'PORT': os.environ.get("DB_PORT"),
         'TEST': {
             'NAME': 'testdb'
         }
