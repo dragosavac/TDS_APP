c8c2bb61b6ccf706d2da0661d43fb912dbe840c0
Rade <rade.dragosavac@etondigital.com>
2021-01-17 11:34:43 +0100

    Additional small fixes

--- tds/views.py
+++ tds/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from utils.helpers import get_users_ip_address, create_user, generate_graph
+from utils.helpers import get_users_ip_address, fill_user_click_stats, fill_user_click_stats_red_to_default
+from utils.helpers import generate_graph
 from .models import ShortenUrl, RedirectLink, UserClickStat
 from django.contrib.gis.geoip2 import GeoIP2
 from django.urls import reverse_lazy
@@ -108,23 +109,31 @@ def redirect_url(request, parameter):
 
         if by_country_qs.exists():
             url = by_country_qs[0]
-            create_user(users_ip_address, shorten_url, parameter, url)
+            same_weight_lp = RedirectLink.objects.filter(
+                parameter=shorten_url,
+                country=location_code,
+                weight=url.weight
+            )
+
+            url = random.choice(same_weight_lp)
+            fill_user_click_stats(users_ip_address, shorten_url, parameter, url)
 
             return redirect(url.url)
         
         elif by_weight_qs.exists():
             url = by_weight_qs[0]
-            create_user(users_ip_address, shorten_url, parameter, url)
+            same_weight_lp = RedirectLink.objects.filter(
+                parameter=shorten_url,
+                country=None,
+                weight=url.weight
+            )
+
+            url = random.choice(same_weight_lp)
+            fill_user_click_stats(users_ip_address, shorten_url, parameter, url)
 
             return redirect(url.url)
 
-        user = UserClickStat()
-        user.ip = users_ip_address
-        user.url_created_by = shorten_url.user
-        user.parameter = parameter
-        user.url = shorten_url.link
-        user.save()
-
+        fill_user_click_stats_red_to_default(users_ip_address, shorten_url, parameter)
         return redirect(shorten_url.link)
 
     except ObjectDoesNotExist:
@@ -177,9 +186,11 @@ def link_stats_view(request, parameter):
     unique_click = user_click_distinct.count()
 
     data_click = []
+    data_updated = []
+    objects_a = []
 
     for i in range(24):
-        hr = i+1
+        hr = i + 1
         time_threshold = timezone.now() - timedelta(hours=hr)
         user_click_24 = UserClickStat.objects.filter(
             parameter=parameter, time__gte=time_threshold,
@@ -189,17 +200,30 @@ def link_stats_view(request, parameter):
         data_click.append(user_click_24.count())
 
     hr24 = data_click[23]
-    
+
     for i in range(24):
+        data_updated.append(0)
         data_click[i] = 0
         data_click[i] = UserClickStat.objects.filter(
-            parameter=parameter, time__hour__gte=i, time__hour__lt=i+1,
+            parameter=parameter, time__hour__gte=i, time__hour__lt=i + 1,
             url_created_by=request.user
         ).count()
-    
+
     short = ShortenUrl.objects.get(
         parameter=parameter
-        )
+    )
+
+    hr_now = timezone.now().hour
+
+    for i in range(24 - hr_now):
+        data_updated[i] = data_click[hr_now + i]
+        objects_a.append(hr_now + i)
+
+    for i in range(hr_now):
+        data_updated[24 - hr_now + i] = data_click[i]
+        objects_a.append(i)
+
+    generate_graph(data_updated, objects_a)
 
     context = {
         "short": short,
@@ -209,7 +233,6 @@ def link_stats_view(request, parameter):
         "hr24": hr24,
         "user_click": user_click_24
     }
-    generate_graph(data_click)
 
     return render(request, "link_stats.html", context)
 
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from tds.models import UserClickStat
 from matplotlib import pyplot as plt
 import numpy as np
@@ -15,7 +17,7 @@ def get_users_ip_address(request):
     return ip
 
 
-def create_user(users_ip, shorten_url, parameter, url):
+def fill_user_click_stats(users_ip, shorten_url, parameter, url):
     user = UserClickStat()
     user.ip = users_ip
     user.url_created_by = shorten_url.user
@@ -24,16 +26,39 @@ def create_user(users_ip, shorten_url, parameter, url):
     user.save()
 
 
-def generate_graph(data_click):
-    objects = []
+def fill_user_click_stats_red_to_default(users_ip, shorten_url, parameter):
+    user = UserClickStat()
+    user.ip = users_ip
+    user.url_created_by = shorten_url.user
+    user.parameter = parameter
+    user.url = shorten_url.link
+    user.save()
 
-    for i in range(24):
-        objects.append(i + 1)
+
+def generate_graph(data_updated, objects_a):
+    hours = [datetime.time(i).strftime('%H') for i in range(24)]
+    objects_display = [hours[objects_a[i]] for i in range(24)]
+
+    data_latest = data_updated[0]
+
+    for i in range(24 - 1):
+        data_updated[i] = data_updated[i + 1]
+
+    data_updated[23] = data_latest
+
+    object_latest = objects_display[0]
+
+    for i in range(24 - 1):
+        objects_display[i] = objects_display[i + 1]
+
+    objects_display[23] = object_latest
 
     plt.clf()
-    y_pos = np.arange(len(objects))
-    plt.bar(y_pos, data_click, align='center', alpha=0.5)
-    plt.xticks(y_pos, objects)
+    y_pos = np.arange(len(objects_display))
+    plt.bar(y_pos, data_updated, align='center', alpha=0.5, width=0.8)
+    plt.xticks(y_pos, objects_display)
+    plt.xlabel('Hours')
     plt.ylabel('Clicks')
     plt.title('Clicks distribution in last 24 hours')
     plt.savefig('media/graph.png')
+
